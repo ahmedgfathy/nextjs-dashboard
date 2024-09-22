9ec9fa9948fed98652f5be2d383a713f5408e634
Ahmed Gomaa <ahmedgfathy@gmail.com>
2024-09-22 10:54:47 +0300

    card,dashboards

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -14,13 +14,13 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard', icon: HomeIcon },
+  { name: 'الصفحة الرئيسية', href: '/dashboard', icon: HomeIcon },
   {
-    name: 'Invoices',
+    name: 'الفواتير',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
+  { name: 'العملاء', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
