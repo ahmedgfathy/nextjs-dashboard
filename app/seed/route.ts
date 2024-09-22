00e2459fd8cb62940ca6505fba8bdc702bbba85e
Ahmed Gomaa <ahmedgfathy@gmail.com>
2024-09-22 08:19:33 +0300

    env.local

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -104,8 +104,8 @@ async function seedRevenue() {
 
 export async function GET() {
   return Response.json({
-   // message:
-    //  'Uncomment this file and remove this line. You can delete this file when you are finished.',
+    message:
+      'Uncomment this file and remove this line. You can delete this file when you are finished.',
   });
   try {
     await client.sql`BEGIN`;
